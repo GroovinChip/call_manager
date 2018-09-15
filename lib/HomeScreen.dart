bbc4c8997ed0760f2bd90723216e56fa7390bbec
GroovinChip <reuben.turner42@gmail.com>
2018-09-14 20:37:57 -0400

    Updated send email icon

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -105,7 +105,7 @@ class _HomeScreenState extends State<HomeScreen> {
               padding: const EdgeInsets.only(right: 8.0),
               child: Text("Send Email"),
             ),
-            Icon(Icons.send),
+            Icon(GroovinMaterialIcons.send_outline),
           ],
         ),
         value: "Send Email",
