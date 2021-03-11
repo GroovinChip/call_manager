7581dda5b4ce1aee69a957f53447c6ca0ccbb459
Reuben Turner <groovinchip@gmail.com>
2021-03-10 20:45:15 -0500

    Remove unused import

--- lib/Login/login_screen.dart
+++ lib/Login/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'dart:async';
-import 'package:groovin_material_icons/groovin_material_icons.dart';
 import 'package:flutter/services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:call_manager/globals.dart' as globals;
