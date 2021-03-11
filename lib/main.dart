4c08ec0074037c8ce18255d8969f037aebd1013e
Reuben Turner <groovinchip@gmail.com>
2021-03-10 20:38:28 -0500

    Organize imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:call_manager/EditCall/edit_call_screen.dart';
-import 'package:call_manager/utils/pass_notification.dart';
+import 'package:call_manager/HomeScreen/home_screen.dart';
 import 'package:call_manager/Login/login_screen.dart';
+import 'package:call_manager/NewCall/add_new_call_screen.dart';
+import 'package:call_manager/utils/pass_notification.dart';
 import 'package:call_number/call_number.dart';
 import 'package:flutter/material.dart';
-import 'package:call_manager/HomeScreen/home_screen.dart';
-import 'package:call_manager/NewCall/add_new_call_screen.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
