ebd2960ac81c3b8a4e9583706205caa64d2ed3cc
Reuben Turner <groovinchip@gmail.com>
2021-03-12 10:17:17 -0500

    Remove unecessary safearea

--- lib/screens/new_call_screen.dart
+++ lib/screens/new_call_screen.dart
@@ -112,217 +112,215 @@ class _NewCallScreenState extends State<NewCallScreen>
         title: Text('New Call'),
         backwardsCompatibility: false,
       ),
-      body: SafeArea(
-        child: Form(
-          key: formKey,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  TypeAheadFormField(
-                    suggestionsCallback:
-                        contactsUtility.searchContactsWithQuery,
-                    itemBuilder: (context, contact) {
-                      return ListTile(
-                        leading: contact.avatar == null ||
-                            contact.avatar.length == 0
-                            ? CircleAvatar(
-                                child: Icon(Icons.person_outline),
-                              )
-                            : ClipRRect(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(25.0)),
-                                child: CircleAvatar(
-                                  child: Image.memory(
-                                    contact.avatar,
-                                  ),
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TypeAheadFormField(
+                  suggestionsCallback:
+                      contactsUtility.searchContactsWithQuery,
+                  itemBuilder: (context, contact) {
+                    return ListTile(
+                      leading: contact.avatar == null ||
+                          contact.avatar.length == 0
+                          ? CircleAvatar(
+                              child: Icon(Icons.person_outline),
+                            )
+                          : ClipRRect(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(25.0)),
+                              child: CircleAvatar(
+                                child: Image.memory(
+                                  contact.avatar,
                                 ),
                               ),
-                        title: Text(contact.displayName),
-                      );
-                    },
-                    transitionBuilder: (context, suggestionsBox, controller) {
-                      return suggestionsBox;
-                    },
-                    onSuggestionSelected: (contact) {
-                      selectedContact = contact;
-                      _nameFieldController.text = selectedContact.displayName;
-                      if (selectedContact.phones.length > 1) {
-                        showModalBottomSheet(
-                          context: context,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
+                            ),
+                      title: Text(contact.displayName),
+                    );
+                  },
+                  transitionBuilder: (context, suggestionsBox, controller) {
+                    return suggestionsBox;
+                  },
+                  onSuggestionSelected: (contact) {
+                    selectedContact = contact;
+                    _nameFieldController.text = selectedContact.displayName;
+                    if (selectedContact.phones.length > 1) {
+                      showModalBottomSheet(
+                        context: context,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        builder: (_) => MultiplePhoneNumbersSheet(
+                          selectedContact: selectedContact,
+                        ),
+                      ).then((value) {
+                        _phoneFieldController.text = value;
+                      });
+                    } else {
+                      _phoneFieldController.text =
+                          selectedContact.phones.first.value;
+                    }
+                  },
+                  validator: (input) => input == null || input == ''
+                      ? 'This field is required'
+                      : null,
+                  onSaved: (contactName) =>
+                      _nameFieldController.text = contactName,
+                  textFieldConfiguration: TextFieldConfiguration(
+                    enabled: true,
+                    textCapitalization: TextCapitalization.words,
+                    controller: _nameFieldController,
+                    keyboardType: TextInputType.text,
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(
+                        OMIcons.person,
+                        color: theme.brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.grey,
+                      ),
+                      suffixIcon: Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.close,
+                            color: Theme.of(context).brightness ==
+                                    Brightness.dark
+                                ? Colors.white
+                                : Colors.grey,
                           ),
-                          builder: (_) => MultiplePhoneNumbersSheet(
-                            selectedContact: selectedContact,
-                          ),
-                        ).then((value) {
-                          _phoneFieldController.text = value;
-                        });
-                      } else {
-                        _phoneFieldController.text =
-                            selectedContact.phones.first.value;
-                      }
-                    },
-                    validator: (input) => input == null || input == ''
-                        ? 'This field is required'
-                        : null,
-                    onSaved: (contactName) =>
-                        _nameFieldController.text = contactName,
-                    textFieldConfiguration: TextFieldConfiguration(
-                      enabled: true,
-                      textCapitalization: TextCapitalization.words,
-                      controller: _nameFieldController,
-                      keyboardType: TextInputType.text,
-                      maxLines: 1,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(
-                          OMIcons.person,
+                          onPressed: () => _nameFieldController.text = '',
+                        ),
+                      ),
+                      labelText: 'Name (Required)',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  validator: (input) => input == null || input == ''
+                      ? 'This field is required'
+                      : null,
+                  onSaved: (input) => _phoneFieldController.text = input,
+                  enabled: true,
+                  keyboardType: TextInputType.phone,
+                  maxLines: 1,
+                  autofocus: false,
+                  controller: _phoneFieldController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      OMIcons.phone,
+                      color: theme.brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.grey,
+                    ),
+                    labelText: 'Phone Number (Required)',
+                    border: OutlineInputBorder(),
+                    suffixIcon: Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.close,
                           color: theme.brightness == Brightness.dark
                               ? Colors.white
                               : Colors.grey,
                         ),
-                        suffixIcon: Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: IconButton(
-                            icon: Icon(
-                              Icons.close,
-                              color: Theme.of(context).brightness ==
-                                      Brightness.dark
-                                  ? Colors.white
-                                  : Colors.grey,
-                            ),
-                            onPressed: () => _nameFieldController.text = '',
-                          ),
+                        onPressed: () => _phoneFieldController.text = '',
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  enabled: true,
+                  keyboardType: TextInputType.multiline,
+                  textCapitalization: TextCapitalization.sentences,
+                  maxLines: 2,
+                  autofocus: false,
+                  controller: _descriptionFieldController,
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                    prefixIcon: Icon(
+                      OMIcons.comment,
+                      color: Theme.of(context).brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.grey,
+                    ),
+                    suffixIcon: Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.close,
+                          color: theme.brightness == Brightness.dark
+                              ? Colors.white
+                              : Colors.grey,
                         ),
-                        labelText: 'Name (Required)',
+                        onPressed: () =>
+                            _descriptionFieldController.text = '',
                       ),
                     ),
+                    border: OutlineInputBorder(),
                   ),
-                  const SizedBox(height: 16.0),
-                  TextFormField(
-                    validator: (input) => input == null || input == ''
-                        ? 'This field is required'
-                        : null,
-                    onSaved: (input) => _phoneFieldController.text = input,
-                    enabled: true,
-                    keyboardType: TextInputType.phone,
-                    maxLines: 1,
-                    autofocus: false,
-                    controller: _phoneFieldController,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        OMIcons.phone,
-                        color: theme.brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.grey,
-                      ),
-                      labelText: 'Phone Number (Required)',
-                      border: OutlineInputBorder(),
-                      suffixIcon: Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: IconButton(
-                          icon: Icon(
-                            Icons.close,
-                            color: theme.brightness == Brightness.dark
-                                ? Colors.white
-                                : Colors.grey,
-                          ),
-                          onPressed: () => _phoneFieldController.text = '',
-                        ),
-                      ),
+                ),
+                const SizedBox(height: 16.0),
+                DateTimeField(
+                  format: dateFormat,
+                  onShowPicker: (context, currentValue) {
+                    return showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime(DateTime.now().year + 1),
+                    );
+                  },
+                  onChanged: (date) {
+                    reminderDate = date;
+                    //_dateFieldController.text = date.toString();
+                  },
+                  controller: _dateFieldController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.today,
+                      color: theme.brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.grey,
                     ),
+                    labelText: 'Reminder Date',
+                    border: OutlineInputBorder(),
                   ),
-                  const SizedBox(height: 16.0),
-                  TextFormField(
-                    enabled: true,
-                    keyboardType: TextInputType.multiline,
-                    textCapitalization: TextCapitalization.sentences,
-                    maxLines: 2,
-                    autofocus: false,
-                    controller: _descriptionFieldController,
-                    decoration: InputDecoration(
-                      labelText: 'Description',
-                      prefixIcon: Icon(
-                        OMIcons.comment,
-                        color: Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.grey,
-                      ),
-                      suffixIcon: Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: IconButton(
-                          icon: Icon(
-                            Icons.close,
-                            color: theme.brightness == Brightness.dark
-                                ? Colors.white
-                                : Colors.grey,
-                          ),
-                          onPressed: () =>
-                              _descriptionFieldController.text = '',
-                        ),
-                      ),
-                      border: OutlineInputBorder(),
+                ),
+                const SizedBox(height: 16.0),
+                DateTimeField(
+                  format: timeFormat,
+                  enabled: true,
+                  onChanged: (timeOfDay) {
+                    reminderTime = TimeOfDay.fromDateTime(timeOfDay);
+                  },
+                  onShowPicker: (context, currentValue) async {
+                    final time = await showTimePicker(
+                      context: context,
+                      initialTime: TimeOfDay.fromDateTime(
+                          currentValue ?? DateTime.now()),
+                    );
+                    return DateTimeField.convert(time);
+                  },
+                  controller: _timeFieldController,
+                  decoration: InputDecoration(
+                    labelText: 'Reminder Time',
+                    prefixIcon: Icon(
+                      Icons.access_time,
+                      color: theme.brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.grey,
                     ),
+                    border: OutlineInputBorder(),
                   ),
-                  const SizedBox(height: 16.0),
-                  DateTimeField(
-                    format: dateFormat,
-                    onShowPicker: (context, currentValue) {
-                      return showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime.now(),
-                        lastDate: DateTime(DateTime.now().year + 1),
-                      );
-                    },
-                    onChanged: (date) {
-                      reminderDate = date;
-                      //_dateFieldController.text = date.toString();
-                    },
-                    controller: _dateFieldController,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.today,
-                        color: theme.brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.grey,
-                      ),
-                      labelText: 'Reminder Date',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  DateTimeField(
-                    format: timeFormat,
-                    enabled: true,
-                    onChanged: (timeOfDay) {
-                      reminderTime = TimeOfDay.fromDateTime(timeOfDay);
-                    },
-                    onShowPicker: (context, currentValue) async {
-                      final time = await showTimePicker(
-                        context: context,
-                        initialTime: TimeOfDay.fromDateTime(
-                            currentValue ?? DateTime.now()),
-                      );
-                      return DateTimeField.convert(time);
-                    },
-                    controller: _timeFieldController,
-                    decoration: InputDecoration(
-                      labelText: 'Reminder Time',
-                      prefixIcon: Icon(
-                        Icons.access_time,
-                        color: theme.brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.grey,
-                      ),
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
