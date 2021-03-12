e4a21619ea04b46cf9674ae365f19a399e5db8f7
Reuben Turner <groovinchip@gmail.com>
2021-03-12 08:56:21 -0500

    Call data model

---
+++ lib/data_models/call.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/foundation.dart';
+
+class Call {
+  Call({
+    @required this.id,
+    @required this.name,
+    @required this.phoneNumber,
+    this.avatar,
+    this.description,
+    this.reminderDate,
+    this.reminderTime,
+  });
+
+  factory Call.fromJsonWithDocId(Map<String, dynamic> json, String docId) {
+    return Call(
+      id: docId,
+      name: json['Name'],
+      phoneNumber: json['PhoneNumber'],
+      avatar: json['avatar'] ?? '',
+      description: json['Description'],
+      reminderDate: json['ReminderDate'],
+      reminderTime: json['ReminderTime'],
+    );
+  }
+
+  final String id;
+  final String name;
+  final String phoneNumber;
+  final String avatar;
+  final String description;
+  final String reminderDate;
+  final String reminderTime;
+
+  bool get hasAvatar => avatar.isNotEmpty;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'Call': {
+        'id': id,
+        'name': name,
+        'phone': phoneNumber,
+        'hasAvatar': avatar.isNotEmpty,
+        'description': description,
+        'reminderDate': reminderDate,
+        'reminderTime': reminderTime,
+      }
+    };
+  }
+}
