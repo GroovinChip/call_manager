6e946b686195396800fefa50379d21bffb7a30be
Reuben Turner <groovinchip@gmail.com>
2021-03-15 23:31:30 -0400

    Update to Android Embedding v2

--- android/app/src/main/java/com/groovinchip/flutter/callmanager/MainActivity.java
+++ android/app/src/main/java/com/groovinchip/flutter/callmanager/MainActivity.java
@@ -1,17 +1,7 @@
 package com.groovinchip.flutter.callmanager;
 
-import android.os.Bundle;
-
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-      /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-          getWindow().setStatusBarColor(0x00000000);
-      }*/
-      GeneratedPluginRegistrant.registerWith(this);
-  }
+
 }
